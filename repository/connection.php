1d06a32679517ccd9e3bc0762161066099507483
Walid Asakak <asakakwalid@gmail.com>
2026-02-08 10:09:10 +0000

    feat(db): Add connection.php to handle database connection thanks to a function

---
+++ repository/connection.php
@@ -0,0 +1,19 @@
+<?php 
+
+function connectToDataBase() {
+    try {
+        $db = new PDO (
+            'mysql:host=db.3wa.io;port=3306;dbname=walidasakak_to-do-list;charset=utf8',
+            'walidasakak',
+            '11069fbca0a4e3afa84c127a191507e0'
+        );
+        
+        $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $db -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $db;
+    }
+    
+    catch(Exception $e){
+        die($e -> getMessage());
+    }
+}
