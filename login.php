561107f3f2594d34912b91765a1b2ba88977f2d5
arthurlbrr <arthur.le-berre@3wa.io>
2026-02-08 22:51:16 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,37 +1,25 @@
 <?php
-
-session_start();
-
-include('repository/userRepository.php');
+require_once 'repository/connection.php';
 
 $error = '';
 
-// If the user is already connected -> go on user page 
-if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] === true) {
-    header("Location: userAccount.php");
-    exit();
-}
-
-
-if (!empty($_POST)) {
-    $email = $_POST['email'] ?? '';
-    $password = $_POST['password'] ?? '';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = trim($_POST['username']);
+    $password = $_POST['password'];
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->execute([$username]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
     
-    $user = getUserByEmail($email);
-    
-    // Check if user password is correct & if user  exists (adress email)
     if ($user && password_verify($password, $user['password'])) {
-        $_SESSION['loggedIn'] = true;
-        $_SESSION['user'] = $user;
-        header("Location: userAccount.php");
-        exit();
-    } 
-    
-    else {
-            $error = 'Email or password is incorrect.';
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        header('Location: user.php');
+        exit;
+    } else {
+        $error = "Nom d'utilisateur ou mot de passe incorrect";
     }
 }
-
 $layoutTitle = '3WA Tasks - Login';
 $template = 'login.phtml';
 include 'layout.phtml';
+?>
