c9d8a4d1df8dea1e616d04641f4a87b36c3a64f2
arthurlbrr <arthur.le-berre@3wa.io>
2026-02-08 19:04:33 +0100

    Add login functionality with user authentication

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'config.php';
+
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = trim($_POST['username']);
+    $password = $_POST['password'];
+    $stmt = $pdo->prepare("SELECT * FROM user WHERE username = ?");
+    $requete->execute([$username]);
+    $user = $requete->fetch(PDO::FETCH_ASSOC);
+    
+    if ($user && password_verify($password, $user['password'])) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        header('Location:');
+        exit;
+    } else {
+        $error = "Nom d'utilisateur ou mot de passe incorrect";
+    }
+}
+$layoutTitle = '3WA Tasks - Login';
+$template = 'login.phtml';
+include 'layout.phtml';
+?>
