26c56d35e95a7b384bef3c301bb1d8c35d424934
arthurlbrr <arthur.le-berre@3wa.io>
2026-02-08 19:35:02 +0100

    Update login.php

--- login.php
+++ login.php
@@ -6,9 +6,9 @@ $error = '';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = trim($_POST['username']);
     $password = $_POST['password'];
-    $stmt = $pdo->prepare("SELECT * FROM user WHERE username = ?");
-    $requete->execute([$username]);
-    $user = $requete->fetch(PDO::FETCH_ASSOC);
+    $stmt = $pdo->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->execute([$username]);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
     
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
