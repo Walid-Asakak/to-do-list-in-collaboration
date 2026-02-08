b5e4ad58809cb8fe7ed04add3855bda8686f991e
arthurlbrr <arthur.le-berre@3wa.io>
2026-02-08 19:30:13 +0100

    Update login.php

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['username'] = $user['username'];
-        header('Location:');
+        header('Location: user.php');
         exit;
     } else {
         $error = "Nom d'utilisateur ou mot de passe incorrect";
